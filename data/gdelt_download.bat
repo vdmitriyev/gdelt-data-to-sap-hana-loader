c65d7edb55324bd126cfa87abe0d92e51b50cbf0
vdmitriyev <dmitriyev.viktor@gmail.com>
2014-03-19 11:48:00 +0100

    Structure of the project is reorganized. PyCharm usage as primary IDE.

--- data/gdelt_download.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM	@author Viktor Dmitriyev
-REM python gdelt_download_daily.py fetch -d . -U
-python gdelt_download_daily.py fetch_from_date -d . -U -F 20140101
-REM python gdelt_download_daily.py fetch_from_date -d . -U -F 20140305
-pause
