b18c89dbd4843d3b0f5fff7afcd7464e76a775be
vdmitriyev <dmitriyev.viktor@gmail.com>
2015-02-20 17:48:58 +0100

    Updating the .hdbtable for the newer version of SAP HANA - were some issues with comments for the table and fields.

--- src/run.bat
+++ src/run.bat
@@ -2,6 +2,6 @@
 REM	@author Viktor Dmitriyev
 echo "Loading GDELT data into SAP HANA DB ..."
 REM c:\Soft\sap\hdbclient\Python\python.exe gdelt_data_loader.py
-c:\Soft\sap\hdbclient\Python\python.exe gdelt_data_loader.py > generated_output.txt
+c:\Soft\sap\rev80\hdbclient\Python\python.exe gdelt_data_loader.py > generated_output.txt
 REM c:\Soft\sap\hdbclient\Python\python.exe -m cProfile gdelt_data_loader.py > generated_queries.txt
 pause
