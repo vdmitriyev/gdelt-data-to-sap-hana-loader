d54fa2576b6bbd5e4cb5e62e93ba988a0a7a3b23
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2016-09-06 14:30:00 +0200

    Updating styles of the code.

--- src/folder_iterator.py
+++ src/folder_iterator.py
@@ -1,33 +1,36 @@
 ﻿# coding: utf-8
-import os
+#!/usr/bin/env python
 
-"""
-    Author  : Viktor Dmitriyev
-    Goal 	: Helper script that iterates through specified folder and extracts all file names.
-   	Note   	: 
-    Date    : 30.07.2013
-"""
+__author__      = "Viktor Dmitriyev"
+__copyright__   = "Copyright 2016, Viktor Dmitriyev"
+__credits__     = ["Viktor Dmitriyev"]
+__license__     = "MIT"
+__version__     = "1.0.0"
+__email__       = ""
+__status__      = "test"
+__date__        = "30.07.2013"
+__description__ = "Helper script that iterates through specified folder and extracts all file names."
 
 class FolderIterator():
 
-	def iterate_through_catalog(self, rootdir=None):
-		""" (str) -> (dict, dict)
-		
-			Iterating through the given catalog to identify notes.
-		"""
+    def iterate_through_catalog(self, rootdir=None):
+        """ (str) -> (dict, dict)
 
-		if rootdir is None:
-			rootdir = sys.argv[1]
+            Iterating through the given catalog to identify notes.
+        """
 
-		# notes_papers = dict()
-		total_papers = dict()
+        if rootdir is None:
+            rootdir = sys.argv[1]
 
-		for root, _, files in os.walk(rootdir):		
-			for f in files:
-				# if self.is_publication(f):
-				if (f[-4:].lower() in ('.csv')):
-					if root not in total_papers:
-						total_papers[root] = list()
-					total_papers[root].append(f)
+        # notes_papers = dict()
+        total_papers = dict()
 
-		return total_papers
+        for root, _, files in os.walk(rootdir):
+            for f in files:
+                # if self.is_publication(f):
+                if (f[-4:].lower() in ('.csv')):
+                    if root not in total_papers:
+                        total_papers[root] = list()
+                    total_papers[root].append(f)
+
+        return total_papers
